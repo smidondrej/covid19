d522a37a082c3a6c2386ecf669dfc1fa5fbcb3d6
Ondrej Smid <ondrejsmid33@gmail.com>
2020-12-29 23:11:23 +0100

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDmdwrvliu_2pDO59RUe4La3zXMyH9qx-Q",
+    authDomain: "covid19-43280.firebaseapp.com",
+    databaseURL: "https://covid19-43280.firebaseio.com",
+    projectId: "covid19-43280",
+    storageBucket: "covid19-43280.appspot.com",
+    messagingSenderId: "670834274495",
+    appId: "1:670834274495:web:4ba6791669d0bd57b8960a",
+    measurementId: "G-L69G96F9HQ"
+  }
 };
